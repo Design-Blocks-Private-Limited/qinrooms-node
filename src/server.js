af7bea2375369559833df7ee02ddfd57346a1ad5
deepak-pattimi <pattimideepak777@gmail.com>
2026-03-23 10:19:20 +0530

    node v0.0.17

--- src/server.js
+++ src/server.js
@@ -69,7 +69,7 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-// ✅ CHANGE app.listen to server.listen
+
 server.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
