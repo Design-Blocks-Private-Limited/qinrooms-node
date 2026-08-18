72c619cc5feee31a774d6b696356c424274688b8
Deepak <pattimideepak777@gmail.com>
2026-08-18 12:28:21 +0530

    Fix CORS preflight and allow origins dynamically in server.js

--- src/server.js
+++ src/server.js
@@ -77,7 +77,13 @@ io.on("connection", (socket) => {
   });
 });
 
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin']
+}));
+app.options('*', cors());
 app.disable('etag');
 
 app.use(helmet({
