7d33292191aecb5fa3c2924536ea24fe7be4d406
Deepak <pattimideepak777@gmail.com>
2026-08-27 13:34:49 +0530

    v0.0.42

--- src/server.js
+++ src/server.js
@@ -78,7 +78,7 @@ app.use(cors({
   origin: true,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin', 'x-admin-otp', 'X-Admin-OTP']
 }));
 app.disable('etag');
 
